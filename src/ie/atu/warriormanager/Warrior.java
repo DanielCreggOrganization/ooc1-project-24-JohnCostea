77186f45269a05fb71b0ddadded22e8440b655d6
Ionut Costea <john_ionut86@yahoo.com>
2024-11-21 16:17:15 +0000

    added getters and setters

--- src/ie/atu/warriormanager/Warrior.java
+++ src/ie/atu/warriormanager/Warrior.java
@@ -2,7 +2,7 @@ package ie.atu.warriormanager;
 
 public class Warrior {
     //instance variables
-    private String warriorId;  //unique ID
+    private int warriorId;  //unique ID
     private String warriorName;
     private String warriorType;
     private int warriorDamage;
@@ -10,5 +10,48 @@ public class Warrior {
     private boolean isAlive;
     private double distanceToEnemy;
 
+    //getter and setter Methods
+    public int getWarriorId(){
+        return this.warriorId;
+    }
+    public String getWarriorName(){
+        return this.warriorName;
+    }
+    public String getWarriorType(){
+        return this.warriorType;
+    }
+    public int getWarriorDamage(){
+        return this.warriorDamage;
+    }
+    public int getWarriorLife(){
+        return this.warriorLife;
+    }
+    public boolean getIsAlive(){
+        return this.isAlive;
+    }
+    public double getDistanceToEnemy(){
+        return this.distanceToEnemy;
+    }
 
+    public void setWarriorId(){
+        this.warriorId = warriorId;
+    }
+    public void setWarriorName(){
+        this.warriorName = warriorName;
+    }
+    public void setWarriorType(){
+        this.warriorType = warriorType;
+    }
+    public void setWarriorDamage(){
+        this.warriorDamage = warriorDamage;
+    }
+    public void setWarriorLife(){
+        this.warriorLife = warriorLife;
+    }
+    public void setIsAlive(){
+        this.isAlive = isAlive;
+    }
+    public void setDistanceToEnemy(){
+        this.distanceToEnemy = distanceToEnemy;
+    }
 }
