6cb84abe9fa66ec4be2fb30d8f2e4909b8efb7ca
Ionut Costea <john_ionut86@yahoo.com>
2024-11-21 16:00:58 +0000

    added instance variables

--- src/ie/atu/warriormanager/Warrior.java
+++ src/ie/atu/warriormanager/Warrior.java
@@ -1,5 +1,14 @@
 package ie.atu.warriormanager;
 
 public class Warrior {
+    //instance variables
+    private String warriorId;  //unique ID
+    private String warriorName;
+    private String warriorType;
+    private int warriorDamage;
+    private int warriorLife;
+    private boolean isAlive;
+    private double distanceToEnemy;
+
 
 }
