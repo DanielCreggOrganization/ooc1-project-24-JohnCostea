0646bbf9bf67a2ab52104c8cab054acfb9b76057
Ionut Costea <john_ionut86@yahoo.com>
2024-12-11 23:52:03 +0000

    added user selection

--- src/ie/atu/warriormanager/Main.java
+++ src/ie/atu/warriormanager/Main.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         int userSelection = 0;
-        Scanner userInput = new Scanner(System.in);
         ArraysClass aco = new ArraysClass();
-        
+        Scanner userInput = new Scanner(System.in);
         Enemy E1 = new Enemy(102, "Destroyer", 30, 100, true, 80);
         
-        while(true){
+        while(true){          
             System.out.println("\n#####################################");
             System.out.println("#             My RPG Game            #");
             System.out.println("#              Main Menu             #");
@@ -20,28 +19,32 @@ public class Main {
             System.out.println("(4) Attack enemy");
             System.out.println("(5) Quit");
             System.out.println("Select an option from 1 to 5 and press Enter");
-            userSelection = userInput.nextInt();
-
+            
+            userSelection = userInput.nextInt();           
             if (userSelection == 5){
-                //Quit
-                System.out.println("Game closing... Bye");
                 userInput.close();
+                //Quit
+                System.out.println("Game closing... Bye");                
                 break;
+                
             }
             else if (userSelection == 1){
                 Warrior W1 = new Warrior();
-                System.out.println("Please enter a name for your Warrior: ");                       
-                                String userNameChoice = userInput.nextLine();                   
+                System.out.println("Please enter a name for your Warrior: ");
+                        Scanner userName = new Scanner(System.in);       
+                            String userNameChoice = userName.nextLine();                   
                                 W1.setPlayerName(userNameChoice);
-                                userInput.close();
-                System.out.println("Please enter life  for your Warrior(a number from 50 to 100): ");
-                                userSelection = userInput.nextInt();
-                                W1.setPlayerLife(userSelection);                
-                                userInput.close();                             
-                                System.out.println(W1.getPlayerName() + " has " + W1.getPlayerLife() + " life");          
-                                }// end if
-                } //end while
-                                                    
+                                userName.close();
                                 
+                        Scanner userLife = new Scanner(System.in);
+                            int userChoiceLife = userLife.nextInt();
+                                W1.setPlayerLife(userChoiceLife);
+                                userLife.close();
+
+                        System.out.println(W1.getPlayerName() + " has " + W1.getPlayerLife() + " life");
+
+                                break;          
+                                }// end if
+                } //end while                                                                                    
         } // end main
 } //end of main class
--- src/ie/atu/warriormanager/UtilMethods.java
+++ src/ie/atu/warriormanager/UtilMethods.java
@@ -1,8 +1,5 @@
 package ie.atu.warriormanager;
 
 public class UtilMethods {
-    public static void clearScreen() {  
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();  
-    }  
+    
 }
